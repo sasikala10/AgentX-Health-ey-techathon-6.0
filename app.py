e8643dc945e69fb4d712346619b7abaf3f62d7ad
sasikala10 <lathikaekambaram@gmail.com>
2025-12-17 18:12:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ if st.session_state.page == "Home":
 
     st.markdown(
         "<div style='text-align:center;margin-top:15px;'>"
-        "<img src='https://nividous.com/blogs/how-agentic-ai-is-transforming-healthcare-diagnostics/' "
+        "<img src='https://static.vecteezy.com/system/resources/previews/034/637/931/large_2x/a-futuristic-robotic-doctor-in-a-doctor-s-coat-with-a-stethoscope-on-a-blue-background-co'ncept-of-advanced-medical-technology-ai-generative-photo.jpg' "
         "width='160' class='doctor-img'/></div>",
         unsafe_allow_html=True
     )
